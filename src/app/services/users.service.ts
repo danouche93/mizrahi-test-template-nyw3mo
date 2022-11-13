55af2d04123d8f7b4d1b19619100c7a98fa62628
danouche93 <50110805+danouche93@users.noreply.github.com>
2022-11-14 00:43:22 +0200

    1 file modified

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
 
 @Injectable()
 export class UsersService {
-  private _jsonURL = './data/MOCK_DATA.json';
-  data: string;
+  private _jsonURL = 'https://mizrahi-test-template-nyw3mo.stackblitz.io/src/app/data/MOCK_DATA.json';
+  data: any;
 
   constructor(private http: HttpClient) {
-    this.http.get(this._jsonURL, { responseType: 'text' }).subscribe((data) => {
+    this.http.get(this._jsonURL).subscribe((data: any) => {
       this.data = data;
     });
   }
